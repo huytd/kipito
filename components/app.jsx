0aadaf6d12c669572dd76052c067ba7b009ae33b
Huy Tran <kingbazoka@gmail.com>
2016-08-01 23:41:33 -0700

    Reactify the project :omg:

--- components/app.jsx
+++ components/app.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      menu: false
+    }
+
+    this.toggleMenu = this.toggleMenu.bind(this)
+  }
+
+  toggleMenu() {
+    var self = this
+    self.setState({ menu: !self.state.menu });
+    console.log(this.state.menu);
+  }
+
   render() {
+    var self = this;
+    var sideBarClass = 'sidebar ' + ((self.state.menu) ? 'show' : '');
+    console.log('class: ', sideBarClass);
     return (
-        <div className='box'>yo!</div>
+        <div className="container">
+          <div className={sideBarClass}></div>
+          <div className="editor">
+            <div className="header">
+              <a className="menu-btn" onClick={self.toggleMenu}><i>≡</i> Menu</a>
+            </div>
+            <div className="content">
+              <div className="edit-zone">
+                <h1>Text title here</h1>
+                <p>It's awesome!!!</p>
+              </div>
+            </div>
+          </div>  
+        </div>
         )
   }
 }
